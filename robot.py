0b3a8f9f7898014def5cb3206f8855607dad8c24
Xenliath <ethan.ferreiraduarte@gmail.com>
2022-02-09 12:40:18 +0100

    Creation du robot

---
+++ robot.py
@@ -0,0 +1,16 @@
+class Robot():
+
+    def __init__(self, client):
+        """
+        client is used to issue orders
+        """
+        self.client = client
+
+    def process_candle(self, candle_msg:str):
+        """This function is called when a new candle_msg is received.
+            Candle message is a string of the form:
+            'symbol_key' : {'c': [174.3], 'h': [174.3], 'l': [174.19], 'o': [174.19], 's': 'ok', 't': [1643670000], 'v': [1888]}
+
+            Note that there are list, so you can have multiple candles in one message.
+        """
+       
